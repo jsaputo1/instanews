00bb1726fb706280d0924b21b76d744b0bc8abfa
John Saputo <John@John-MBP.local>
2020-02-09 14:48:52 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require ("gulp")
+
+const terser = require ("gulp-terser")
+rename = require("gulp-rename")
+sass = require("gulp-sass")
+cssnano = require ("gulp-cssnano")
+autoprefixer = require("gulp-autoprefixer")
+browserSync = require("browser-sync")
+
+gulp.task("default", function(done){
+    return gulp
+    .src("./js/*.js") // input files for gulp to minify
+    .pipe (terser()) // calls the terser function on these files
+    .pipe (rename({extname: ".min.js"})) //renames minifie dfile
+    .pipe (gulp.dest("./build/js")) //creates a build folder and places min files in there
+    .pipe (browserSync.stream())
+})
+
+
+//This is for SASS, check your file-paths
+gulp.task ('sass', function() {
+    return gulp
+    //locates scss files
+    .src('./css/style.scss')
+    //converts sass to css
+    .pipe(sass())
+    //adds prefixes for compatability
+    // .pipe (
+    //     autoprefixer ({
+    //         browsers: ['last 2 versions'],
+    //     })
+    // )
+
+    //adds css to build dir
+    .pipe(gulp.dest('./build/css'))
+    //minifies our css
+    .pipe (cssnano())
+    //renames our css
+    .pipe(rename('style.min.css'))
+    //adds our final output to build dir
+    .pipe (gulp.dest('./build/css'))
+    //syncs browser whenever a change is made and saved
+    .pipe(browserSync.stream())
+})
+
+gulp.task('watch', function () {
+    browserSync.init({
+        server:{
+            baseDir: './'
+        }
+    })
+
+    gulp.watch('js/*.js', gulp.series(['scripts']))
+    gulp.watch('./*html').on('change', browseerSync.reload)
+
+
+})
+
+gulp.task("default", gulp.series('watch')())
+
+
