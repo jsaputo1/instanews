fa2532e771c8d1fd727490e2155361c7009a2ae7
John Saputo <John@John-MBP.local>
2020-02-13 14:01:43 -0500

    style: changed animations and added loader gif

--- js/main.js
+++ js/main.js
@@ -6,37 +6,37 @@ $(function() {
 
     let userInput = $("select").val();
 
-    if (userInput === "sections") {
-        $("header").removeClass("header-top");
-        $("header").addClass("header-top");
-      
-      }
+    //Change cursor to waiting
+    $("body").addClass("cursor");
+    $(".main-content").addClass("cursor");
 
-      
-    //Move footer back to bottom
+    //Add loader image
+    $("header").append(`<img src="./images/loader.gif" / class="loader">`);
+
+    //Move footer back to bottom if the user chooses sections and hide loader gif
     if (userInput === "sections") {
       $("footer").addClass("bottom-text");
+      $(".loader").hide();
+      $("body").removeClass("cursor");
+      $("main-content").removeClass("cursor");
     }
 
     // console.log (userInput)
 
+    //API
     $.ajax({
       dataType: "json",
       method: "GET",
       url: `https://api.nytimes.com/svc/topstories/v2/${userInput}.json?api-key=qZeF0WlVIE49aaMuTGpSM5bAxYeDLSyT`
-    })
-    .done(function({ results }) {
+    }).done(function({ results }) {
       $.each(results, function(index, value) {
         // console.log(index, value);
-        // console.log (value)
+        // console.log(value);
 
         //Variables
         let articleImage = value.multimedia[0].url;
         let url = value.url;
         let description = value.abstract;
-
-
-        
 
         //Max listing
         if (index === 12) return false;
@@ -44,62 +44,59 @@ $(function() {
         //Remove fixed footer text
         $("footer").removeClass("bottom-text");
 
-        //Display articles
+        //Change back to default cursor
+        $("body").removeClass("cursor");
+        $(".main-content").removeClass("cursor");
 
+        //Display articles
         $(".main-content").append(`
                 <figure class="article-image">
                     <img src="${articleImage}">
-                    <a href="${url}" target="new">
-                    <p class="article-text">${description}<p></a>
-                </figure>`)
+                    <a href="${url}" target="new"><p class="article-text">${description}</p></a>
+                </figure>`);
+        //Backup text if the description is blank
+        if (description === "") {
+          $(".article-text").append(`${value.title}`);
+          if (value.title === "") {
+            $(".article-text").append("Click Here to view the article");
+          }
+        }
+      }); //close for each loop
 
-                if (description === "") {
-                    $(".article-text").append(`${value.title}`)
-                   }
+      //Hide Loader
+      $(".loader").hide();
 
-        //Remove previous articles
-        $(".selection").on("change", function() {
-          $(".main-content").empty();
-          $("footer").addClass("bottom-text")
-        });
-
-        //Move header to the top
-        $("header").addClass("header-top");
-
-        //Adjust tablet/desk logo size
-        $(".logo").addClass("logo-size");
-
-        //toggle
-
-        $(".article-text").hide();
-
-        // $("p").hide();
-
-        // $(".article-image").on("click",function(){
-        //     $(this).siblings().children("p").toggle()
-        //     console.log("test5")
-        // })
-
-        //slide toggle
-        // $(".article-image").mouseenter(function(){
-        //     $(this).find('p').stop(true, true).slideToggle()
-        //     console.log("test6")
-        // });
-
-        $(".article-image").mouseenter(function(){
-            $(this).find('p').show()
-            console.log("test6")
-        });
-
-        $(".article-image").mouseleave(function(){
-            $(this).find('p').fadeOut()
-            console.log("test7")
-        });
-
-   
-
-  
+      //Remove previous articles when changing sections
+      $(".selection").on("change", function() {
+        $(".main-content").empty();
+        $("footer").addClass("bottom-text");
       });
-    });
-  }); // close button function
-}); // close
+
+      //Move header to the top
+      $("header").addClass("header-top");
+
+      //Shrink header in tablet and desktop mode
+      $(".logo").addClass("logo-size");
+
+      //Animation to show/hide text on article images
+      $(".article-text").hide();
+
+      //   slide toggle
+      $(".article-image").mouseenter(function() {
+        $(this)
+          .find("p")
+          .slideToggle(500);
+        console.log("test5");
+      });
+
+      $(".article-image").mouseleave(function() {
+        $(this)
+          .find("p")
+          .slideToggle(1000);
+        console.log("test6");
+      });
+
+      
+    }); // close done function
+  }); // close on change function
+}); // close opening function
