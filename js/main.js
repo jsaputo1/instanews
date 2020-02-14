9d6dd9158075fd3cf5bc7df82ff6c89101d5b564
John Saputo <John@John-MBP.local>
2020-02-14 12:09:14 -0500

    chore: removed comments

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
-console.log("working");
-
 $(function() {
-  $(".selection").on("change", function() {
-    // console.log ("test")
+  //Disables fixed footer positioning if the window height is smaller than the main logo
+  if ($(window).height() < 400) {
+    $("footer").removeClass("bottom-text");
+  }
 
+  $(".selection").on("change", function() {
     let userInput = $("select").val();
 
-    //Change cursor to waiting
-    $("body").addClass("cursor");
+    $("body").addClass("cursor"); //Changes cursor to waiting
     $(".main-content").addClass("cursor");
 
-    //Add loader image
-    $("header").append(`<img src="./images/loader.gif" / class="loader">`);
+    $("header").append(`<img src="./images/loader.gif" / class="loader">`); //Loader image
 
-    //Move footer back to bottom if the user chooses sections and hide loader gif
     if (userInput === "sections") {
       $("footer").addClass("bottom-text");
       $(".loader").hide();
       $("body").removeClass("cursor");
       $("main-content").removeClass("cursor");
-    }
-
-    // console.log (userInput)
+    } //Moves footer back to bottom and changes the cursor back to default if the user chooses Sections
 
     //API
     $.ajax({
@@ -30,30 +26,25 @@ $(function() {
       url: `https://api.nytimes.com/svc/topstories/v2/${userInput}.json?api-key=qZeF0WlVIE49aaMuTGpSM5bAxYeDLSyT`
     }).done(function({ results }) {
       $.each(results, function(index, value) {
-        // console.log(index, value);
-        // console.log(value);
+        if (index === 12) return false; //Max listing
 
-        //Variables
+        $("footer").removeClass("bottom-text"); //Remove fixed footer text
+
+        $("body").removeClass("cursor");
+        $(".main-content").removeClass("cursor");
+        //Changes back to default cursor
+
+        //Display articles
         let articleImage = value.multimedia[0].url;
         let url = value.url;
         let description = value.abstract;
 
-        //Max listing
-        if (index === 12) return false;
-
-        //Remove fixed footer text
-        $("footer").removeClass("bottom-text");
-
-        //Change back to default cursor
-        $("body").removeClass("cursor");
-        $(".main-content").removeClass("cursor");
-
-        //Display articles
         $(".main-content").append(`
                 <figure class="article-image">
                     <img src="${articleImage}">
                     <a href="${url}" target="new"><p class="article-text">${description}</p></a>
                 </figure>`);
+
         //Backup text if the description is blank
         if (description === "") {
           $(".article-text").append(`${value.title}`);
@@ -63,40 +54,29 @@ $(function() {
         }
       }); //close for each loop
 
-      //Hide Loader
-      $(".loader").hide();
+      $(".loader").hide(); // Hides loader gif
+      $("header").addClass("header-top"); //Moves header to the top
+      $(".logo").addClass("logo-size"); //Shrinks header in tablet and desktop mode
 
-      //Remove previous articles when changing sections
       $(".selection").on("change", function() {
         $(".main-content").empty();
         $("footer").addClass("bottom-text");
-      });
+      }); //Removes previous articles
 
-      //Move header to the top
-      $("header").addClass("header-top");
+      // Article text box animations
+      $(".article-text").hide(); //Hides article text/link by default
 
-      //Shrink header in tablet and desktop mode
-      $(".logo").addClass("logo-size");
-
-      //Animation to show/hide text on article images
-      $(".article-text").hide();
-
-      //   slide toggle
       $(".article-image").mouseenter(function() {
         $(this)
           .find("p")
           .slideToggle(500);
-        console.log("test5");
       });
 
       $(".article-image").mouseleave(function() {
         $(this)
           .find("p")
           .slideToggle(1000);
-        console.log("test6");
       });
-
-      
     }); // close done function
   }); // close on change function
 }); // close opening function
